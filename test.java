43e20a89c4ffade0df088a4dae389ce4a649e068
Sachin Ojha <sachin.ojha@thoughts2binary.com>
2022-08-14 16:44:36 +0530

    Minor changes

--- test.java
+++ test.java
@@ -1,41 +1,24 @@
 import java.util.*;
 class test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        int res[] = sortArray012(a, n);
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println();
-    }
-    static int[] sortArray012(int a[], int n) {
-        int i = 0;
-        int j = 0;
-        int k = n - 1;
-        while (k >= j) {
-            if (a[j] == 1) {
-                j++;
-            }
-            else if (a[j] == 0) {
-                swap(a, i, j);
-                i++;
-                j++;
-            }
-            else {
-                swap(a, j, k);
-                k--;
-            }
-        }
-        return a;
-    }
-    static void swap(int a[], int l, int m) {
-        int temp = a[l];
-        a[l] = a[m];
-        a[m] = temp;
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		getPaths(0, 0, n, m, "");
+	}
+	static void getPaths(int n, int m, int row, int col, String res) {
+		if (n == row - 1 && m == col - 1) {
+			System.out.print(res + "  ");
+			return;
+		}
+		if (n >= row || m >= col) {
+			return;
+		}
+		for (int i = 1; i <= 3; i++) {
+			getPaths(n, m + i, row, col, res + "H" + i);
+		}
+		for (int i = 1; i <= 3; i++) {
+			getPaths(n + i, m, row, col, res + "V" + i);
+		}
+	}
 }
